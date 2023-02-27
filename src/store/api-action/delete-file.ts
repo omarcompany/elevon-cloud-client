b9225d7c9758a1d8348391d4ecf1f1ddd4afcef4
Aleksey Tatarov <alekseytatarv@gmail.com>
2023-02-27 21:19:55 +0400

    Fix: use correct id data to delete file from state

--- src/store/api-action/delete-file.ts
+++ src/store/api-action/delete-file.ts
@@ -2,16 +2,13 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 import { api, store } from '../store';
 import { removeFileFromState } from '../action';
-import { IFileServer } from '../../interfaces';
-import { adaptFileToClient } from '../../adapter';
 
 export const deleteFile = createAsyncThunk(
   'files/delete',
   async (id: string) => {
     try {
-      const { data } = await api.delete<IFileServer>(`/files/delete/${id}`);
-      const file = adaptFileToClient(data);
-      store.dispatch(removeFileFromState(file.id));
+      const { data } = await api.delete(`/files/delete/${id}`);
+      store.dispatch(removeFileFromState(data.id));
     } catch (error) {
       console.log(error);
     }
